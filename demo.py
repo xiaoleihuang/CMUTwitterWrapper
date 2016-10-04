220fe4eb54ddaf2a78de2fead8e1db81fd839c0e
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2016-10-04 11:57:46 -0600

    add tokenize wrapper in python demo

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
 from py4j.java_gateway import JavaGateway
 #from py4j.java_gateway import GatewayParameters
 
-#gateway = JavaGateway(GatewayParameters(auto_field=True))
+#gateway = JavaGateway(gateway_parameters=GatewayParameters(auto_field=True))
 gateway = JavaGateway()
-wrapper = gateway.entry_point.getWrapper()
-test  = wrapper.parse1tweet('RT @DjBlack_Pearl: wat muhfuckaz wearin 4 the lingerie party?????')
+parserwrapper = gateway.entry_point.getParserWrapper()
+test  = parserwrapper.parse1tweet('RT @DjBlack_Pearl: wat muhfuckaz wearin 4 the lingerie party?????')
 
 print(len(test))
 # have to get access to value by calling methods
 print(test[5].getKey() + "\t" + test[5].getValue())
+
+tokenizerwrapper = gateway.entry_point.getTokenizerWrapper()
+test = tokenizerwrapper.tokenizesSimpleTweet('RT @DjBlack_Pearl: wat muhfuckaz wearin 4    the lingerie party?????')
+print(len(test))
+print(test)
